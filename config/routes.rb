f096a7057f589d2e29f5c3fffa16d0ac8298921a
EC2 Default User <ec2-user@ip-172-31-32-218.ap-northeast-1.compute.internal>
2024-11-24 08:25:37 +0000

    Item46 cycle1 finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'users/index'
   # get 'users/show'
   # get 'users/edit'
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   # get 'books/new'
   # get 'books/index'
   # get 'books/show'
